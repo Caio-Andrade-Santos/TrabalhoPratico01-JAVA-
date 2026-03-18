e2530a97d1c108f04e87b7f431a9411bded4f3ca
AlexandreZampieri <140533916+AlexandreZampieri@users.noreply.github.com>
2026-03-18 20:03:51 -0300

    Add files via upload

---
+++ TP01Ex04.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp01ex04;
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
+public class TP01Ex04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 4. A partir dos valores da base e altura de um triângulo, calcular e exibir sua área.
+            double base, altura, area;
+             Scanner scanner = new Scanner(System.in);
+             
+             System.out.println("Digite a base do triângulo: ");
+             base = scanner.nextDouble();
+             
+             System.out.println("Digite a altura do triângulo: ");
+             altura = scanner.nextDouble();
+             
+             area = (base * altura) / 2;
+             
+             System.out.println("A área do triângulo é: " + area);
+    }
+    
+}
