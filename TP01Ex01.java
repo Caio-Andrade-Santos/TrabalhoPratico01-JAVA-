4db4f56f2a3f888396717b3a98d8480a16b80f74
AlexandreZampieri <140533916+AlexandreZampieri@users.noreply.github.com>
2026-03-18 19:37:39 -0300

    Add files via upload

---
+++ TP01Ex01.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp01ex01;
+
+import java.util.Scanner;
+import java.util.Locale;
+/**
+ *
+ * @author aluno
+ */
+public class TP01Ex01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            /**
+     *     1. Entrar via teclado com a base e a altura de um retângulo, calcular e exibir sua área.
+     */
+        
+        Scanner scanner = new Scanner(System.in);
+        double area, base, altura;
+        
+        System.out.print("Digite a base do triâgulo: ");
+         base = scanner.nextDouble();
+        
+        System.out.print("Digite a base do triâgulo: ");
+         altura = scanner.nextDouble();
+        
+         area = base * altura;
+        
+        System.out.println("A area é: " + area);
+        
+        // 4. Fechar o scanner
+        scanner.close();
+        
+    }
+    
+}
