3a2491e81a0aa3b9d95f2c5d9f5c65e0e8d865ca
Caio-Andrade-Santos <cavians2015@gmail.com>
2026-03-18 19:44:40 -0300

    Add files via upload

---
+++ Exercicio10.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio10;
+import java.util.Scanner;
+
+/**
+ *
+ * @author cavia
+ */
+public class Exercicio10 {
+    /*Nomes: Caio Andrade, Alexandre Zampiere
+            
+    10. Entrar via teclado com o valor de uma temperatura em graus Celsius, calcular e
+exibir sua temperatura equivalente em Fahrenheit. */
+    
+  /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double far = 0;
+       double cel = 0;
+       
+        System.out.println("Digite uma temperatura em celcius");
+        Scanner scan= new Scanner(System.in); // Pode ser reutilizado
+        cel = scan.nextDouble();
+        far = (cel * 1.8) + 32;
+        System.out.println("O valor em Fahrenheit " + far);
+    }
+    
+}
