6f3d51c83da08781c08dc5a6928f7e21caff3d64
AlexandreZampieri <140533916+AlexandreZampieri@users.noreply.github.com>
2026-03-20 19:48:48 -0300

    Update TP01Ex08.java

--- TP01Ex08.java
+++ TP01Ex08.java
@@ -18,6 +18,7 @@ public class TP01Ex08 {
     public static void main(String[] args) {
         // 8. Sabendo que uma milha marítima equivale a um mil, oitocentos e cinquenta e dois metros e que um quilômetro possui mil metros, 
         //fazer um programa para converter milhas marítimas em quilômetros.
+        //Alexandre Zampieri, Caio Andrade 
           Scanner scanner = new Scanner(System.in);
           
           double milhasMvalor = 1852, milhasMaritimas;
