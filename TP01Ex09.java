41138c40fb825c9b2438573f9b536f313d991d07
AlexandreZampieri <140533916+AlexandreZampieri@users.noreply.github.com>
2026-03-20 19:48:19 -0300

    Update TP01Ex09.java

--- TP01Ex09.java
+++ TP01Ex09.java
@@ -18,6 +18,7 @@ public class TP01Ex09 {
     public static void main(String[] args) {
         // 9. Calcular e exibir a tensão de um determinado circuito eletrônico a partir dos valores da resistência e corrente elétrica que serão digitados.
         //Utilize a lei de Ohm.
+        //Alexandre Zampieri, Caio Andrade 
         double corrente, res, tensao;
             Scanner scanner = new Scanner(System.in);
             
